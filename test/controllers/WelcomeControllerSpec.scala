2172e0888fac9ab9d61f84e228f47668e83202c7
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-04-10 13:30:57 +0100

    - Session 4 begin
    - Added Unit test for Afternoon / Morning greeting
    - Modified welcome.scala.html to have Morning Greeting

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -15,7 +15,6 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
     }
   }
   "response to the /welcome url" in {
-
     val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
     val home = route(app, request).get
     status(home) mustBe OK
@@ -25,13 +24,17 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
     val result = controller.welcome.apply(FakeRequest())
     contentType(result) mustBe Some("text/html")
   }
-
-  "return a page which says hello has a title" in {
+  "say good morning and have a title" in {
     val controller = new WelcomeController
-    val result = controller
-                 .welcome()
-                 .apply(FakeRequest(GET, "/welcome"))
-    contentAsString(result) must include("<h1>Hello</h1>")
-    contentAsString(result) must include("<title>Welcome</title>")
+    val result = controller.welcome().apply(FakeRequest())
+    contentAsString(result) must include ("<h1>Good morning!</h1>")
+    contentAsString(result) must include ("<title>Welcome!</title>")
+  }
+  "say good afternoon when it's the afternoon and have a title" ignore {
+    val controller = new WelcomeController
+    val result = controller.welcome().apply(FakeRequest())
+    contentAsString(result) must not include ("<h1>Good morning!</h1>")
+    contentAsString(result) must include ("<h1>Good afternoon!</h1>")
+    contentAsString(result) must include ("<title>Welcome!</title>")
   }
 }
